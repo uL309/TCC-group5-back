0cc393da11636ade01e32b7b9c2c7115d9c9521c
Rafael Felipe <rafa_el_felipe@hotmail.com>
2025-04-27 16:50:33 -0300

    Refactor EngenheiroController to use @Controller annotation and update LoginController to return user name in response. Modify ResponseDTO to reflect name instead of username.

--- src/main/java/puc/airtrack/airtrack/Engenheiro/EngenheiroController.java
+++ src/main/java/puc/airtrack/airtrack/Engenheiro/EngenheiroController.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import puc.airtrack.airtrack.Login.User;
@@ -22,7 +22,7 @@ import puc.airtrack.airtrack.Login.UserService;
 
 
 
-@RestController
+@Controller
 public class EngenheiroController {
 
     @Autowired
--- src/main/java/puc/airtrack/airtrack/Login/ResponseDTO.java
+++ src/main/java/puc/airtrack/airtrack/Login/ResponseDTO.java
@@ -2,5 +2,5 @@ package puc.airtrack.airtrack.Login;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record ResponseDTO(@JsonProperty("name") String username, @JsonProperty("token") String JWT) {
+public record ResponseDTO(@JsonProperty("name") String name, @JsonProperty("token") String token) {
 }
