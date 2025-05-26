fc05981ed2eb44b137111d67088bda726c4f57dc
Gui <guilherme.ioshii@pucpr.edu.br>
2025-05-26 14:29:00 -0300

    Atualizado o Controller do Motor.

--- src/main/java/puc/airtrack/airtrack/Motor/MotorController.java
+++ src/main/java/puc/airtrack/airtrack/Motor/MotorController.java
@@ -6,35 +6,30 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/motores")
 public class MotorController {
 
     @Autowired
     private MotorRepository motorRepository;
 
-    @GetMapping
-    public List<Motor> listarTodos() {
+    @PostMapping("/cmotor")
+    public String cadastrar(@RequestBody Motor motor) {
+        motorRepository.save(motor);
+        return "Motor cadastrado com sucesso!";
+    }
+
+    @GetMapping("/gmotores")
+    public List<Motor> listar() {
         return motorRepository.findAll();
     }
 
-    @GetMapping("/{id}")
-    public Optional<Motor> buscarPorId(@PathVariable int id) {
+    @PutMapping("/umotor")
+    public String atualizar(@RequestBody Motor motor) {
+        motorRepository.save(motor);
+        return "Motor atualizado com sucesso!";
+    }
+
+    @GetMapping("/gmotor")
+    public Optional<Motor> buscarPorId(@RequestParam("param") Integer id) {
         return motorRepository.findById(id);
-    }
-
-    @PostMapping
-    public Motor criar(@RequestBody Motor motor) {
-        return motorRepository.save(motor);
-    }
-
-    @PutMapping("/{id}")
-    public Motor atualizar(@PathVariable int id, @RequestBody Motor motor) {
-        motor.setId(id);
-        return motorRepository.save(motor);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deletar(@PathVariable int id) {
-        motorRepository.deleteById(id);
     }
 }
